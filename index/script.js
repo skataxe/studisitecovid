f9ceb1cd1915de6a9996ea6c79c32d7282ec0dc0
garcia axel <daxel.ag@gmail.com>
2022-03-11 11:27:48 +0100

    1st commit

---
+++ index/script.js
@@ -0,0 +1,14 @@
+
+//let el = document.querySelector(".containerQuestionVaccin")[0];
+//let reponse = document.querySelector(".containerRéponseVaccin")[0];
+
+
+//el.addEventListener("click", () => {containerRéponseVaccin.style.display = "block";});
+//reponse.addEventListener("mouseout", () => {containerRéponseVaccin.display = "none";});
+
+
+//let containerQuestionVaccin = document.querySelector(".containerQuestionVaccin");
+//let réponseVaccin = document.querySelector(".containerRéponseVaccin");
+
+
+//containerQuestionVaccin.addEventListener("mouseover", () => {containerRéponseVaccin.classList.toggle(is-visible)});
